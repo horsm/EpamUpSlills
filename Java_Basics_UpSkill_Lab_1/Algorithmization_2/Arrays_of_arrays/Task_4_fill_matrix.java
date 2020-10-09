99ef359c921905f65e25dc370e6d885126d668d5
horsm <horsm@mail.ru>
2020-10-09 17:59:30 +0300

    решение задачи 4

---
+++ Java_Basics_UpSkill_Lab_1/Algorithmization_2/Arrays_of_arrays/Task_4_fill_matrix.java
@@ -0,0 +1,30 @@
+package Java_Basics_UpSkill_Lab_1.Algorithmization_2.Arrays_of_arrays;
+
+// 4. Сформировать квадратную матрицу порядка n по заданному образцу(n - четное):
+
+public class Task_4_fill_matrix {
+    public static void main(String[] args) {
+        int n = 10;
+        int count = 1;
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i % 2 == 0) {
+                    matrix[i][j] = count++;
+                } else {
+                    matrix[i][j] = n--;
+                }
+            }
+            count = 1;
+            n = 10;
+        }
+
+        for (int[] matrix1 : matrix) {
+            for (int i : matrix1) {
+                System.out.print(i + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
