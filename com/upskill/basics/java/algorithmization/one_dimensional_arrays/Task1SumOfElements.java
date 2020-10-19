c0a8389df224b0346b533465d821449cbfa2df0d
horsm <horsm@mail.ru>
2020-10-19 22:19:27 +0300

    Исправление

--- com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task1SumOfElements.java
+++ com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task1SumOfElements.java
@@ -1,11 +1,14 @@
 package com.upskill.basics.java.algorithmization.one_dimensional_arrays;
 
+// 1. В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
-public class Task_1_sum_of_element {
+public class Task1SumOfElements {
     public static void main(String[] args) {
-
+        Random random = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите размерность массива: ");
         while (!sc.hasNextInt()) {
@@ -20,18 +23,19 @@ public class Task_1_sum_of_element {
             sc.next();
         }
         int k = sc.nextInt();
-
+        int count = 0;
 
         int[] array = new int[n];
         for (int i = 0; i < array.length; i++) {
-            array[i] = ((int) (Math.random() * 100));
-            if (array[i] % k == 0)
-            System.out.print(array[i] + " ");
+            array[i] = random.nextInt(100);
+            if (array[i] % k == 0) {
+                count += array[i];
+            }
         }
 
-        System.out.println();
         System.out.println("Исходный массив");
         System.out.println(Arrays.toString(array));
+        System.out.println("Сумма элементов, которые кратны К = " + count);
     }
 
 }
