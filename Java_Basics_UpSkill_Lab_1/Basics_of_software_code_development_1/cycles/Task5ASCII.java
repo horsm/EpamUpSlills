f62537e9239d50ad9100f880538813451957a22c
horsm <horsm@mail.ru>
2020-10-19 21:32:17 +0300

    Исправление

--- Java_Basics_UpSkill_Lab_1/Basics_of_software_code_development_1/cycles/Task5ASCII.java
+++ Java_Basics_UpSkill_Lab_1/Basics_of_software_code_development_1/cycles/Task5ASCII.java
@@ -1,6 +1,6 @@
 package Java_Basics_UpSkill_Lab_1.Basics_of_software_code_development_1.cycles;
 
-public class Task_5_ASCII {
+public class Task5ASCII {
     public static void main(String[] args) {
         char[] alphabetWithDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
--- Java_Basics_UpSkill_Lab_1/Basics_of_software_code_development_1/cycles/Task6Dividers.java
+++ Java_Basics_UpSkill_Lab_1/Basics_of_software_code_development_1/cycles/Task6Dividers.java
@@ -2,7 +2,7 @@ package Java_Basics_UpSkill_Lab_1.Basics_of_software_code_development_1.cycles;
 
 import java.util.Scanner;
 
-public class Task_6_dividers {
+public class Task6Dividers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите число m: ");
