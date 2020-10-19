75f99643f1835a2069c01bae3e0faf2aab757578
horsm <horsm@mail.ru>
2020-10-19 21:31:25 +0300

    Исправление

--- Java_Basics_UpSkill_Lab_1/Basics_of_software_code_development_1/cycles/Task4MultiOfSquares.java
+++ Java_Basics_UpSkill_Lab_1/Basics_of_software_code_development_1/cycles/Task4MultiOfSquares.java
@@ -1,16 +1,15 @@
 package Java_Basics_UpSkill_Lab_1.Basics_of_software_code_development_1.cycles;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 // 4. Составить программу нахождения произведения квадратов первых двухсот чисел.
+
+import java.math.BigInteger;
 
 public class Task4MultiOfSquares {
     public static void main(String[] args) {
-        int N = 200;
+        int n = 200;
 
         BigInteger result = BigInteger.ONE;
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             result = result.multiply(BigInteger.valueOf(i*i));
         }
         System.out.println(result.toString());
