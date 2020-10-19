1177d06a3728e9c3cb21bc14ec63f91f07a0bc9e
horsm <horsm@mail.ru>
2020-10-19 20:48:45 +0300

    Ошибки исправлены

--- Java_Basics_UpSkill_Lab_1/Basics_of_software_code_development_1/Branching/Task_4_Rectangle.java
+++ Java_Basics_UpSkill_Lab_1/Basics_of_software_code_development_1/Branching/Task_4_Rectangle.java
@@ -1,24 +1,25 @@
 package Java_Basics_UpSkill_Lab_1.Basics_of_software_code_development_1.Branching;
 
+
+
 public class Task_4_Rectangle {
     public static void main(String[] args) {
         // размер кирпича
         int x = 3;
-        int y = 3;
-        int z = 3;
+        int y = 8;
+        int z = 4;
         // размер отверстия
         int a = 4;
-        int b = 2;
+        int b = 3;
 
         if (((x <= a) && (y <= b)) || ((x <= b) && (y <= a))) {
             System.out.println("true");
-        } else if (((x <= a) && (z <= b)) || ((z <= b) && (x <= a))) {
+        } else if (((x <= a) && (z <= b)) || ((z <= a) && (x <= b))) {
             System.out.println("true");
-        } else if (((y <= a) && (z <= b)) || ((z <= b) && (y <= a))) {
+        } else if (((y <= a) && (z <= b)) || ((z <= a) && (y <= b))) {
             System.out.println("true");
         } else
             System.out.println("false");
-
 
     }
 }
