ddcd53266309021bf7bd964e790b576ef12e5e48
horsm <horsm@mail.ru>
2020-10-20 13:24:43 +0300

    Bubble Sort

---
+++ com/upskill/basics/java/algorithmization/sorting/Task3BubbleSort.java
@@ -0,0 +1,43 @@
+package com.upskill.basics.java.algorithmization.sorting;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task3BubbleSort {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите размерность массива: ");
+        while (!sc.hasNextInt()) {
+            System.out.print("Это не число! Введите размерность массива: ");
+            sc.next();
+        }
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        boolean needIteration = true;
+        while (needIteration) {
+            needIteration = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    swap(array, i, i - 1);
+                    needIteration = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void swap(int[] array, int ind1, int ind2) {
+        int tmp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = tmp;
+    }
+}
