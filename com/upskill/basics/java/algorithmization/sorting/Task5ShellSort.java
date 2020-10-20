fb5ae005930df8f0801e3f8400fc18347d7d50e3
horsm <horsm@mail.ru>
2020-10-20 13:41:56 +0300

    Shell Sort

---
+++ com/upskill/basics/java/algorithmization/sorting/Task5ShellSort.java
@@ -0,0 +1,44 @@
+package com.upskill.basics.java.algorithmization.sorting;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task5ShellSort {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите размерность массива: ");
+        while (!sc.hasNextInt()) {
+            System.out.print("Это не число! Введите размерность массива: ");
+            sc.next();
+        }
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        System.out.println(Arrays.toString(array));
+
+
+        int gap = array.length / 2;
+        while (gap >= 1) {
+            for (int right = 0; right < array.length; right++) {
+                for (int c = right - gap; c >= 0; c -= gap) {
+                    if (array[c] > array[c + gap]) {
+                        swap(array, c, c + gap);
+                    }
+                }
+            }
+            gap = gap / 2;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void swap(int[] array, int ind1, int ind2) {
+        int tmp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = tmp;
+    }
+}
