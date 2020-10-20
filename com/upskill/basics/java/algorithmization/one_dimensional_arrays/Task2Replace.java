3e3b0dd11700e3522f9e8394eae8ca44d585f59b
horsm <horsm@mail.ru>
2020-10-20 08:47:18 +0300

    Исправление

--- com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task2Replace.java
+++ com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task2Replace.java
@@ -1,11 +1,15 @@
 package com.upskill.basics.java.algorithmization.one_dimensional_arrays;
 
+// 2. Дана последовательность действительных чисел а1 ,а2 ,..., ап.
+//    Заменить все ее члены, большие данного Z, этим числом. Подсчитать количество замен.
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Task_2_replace {
+public class Task2Replace {
     public static void main(String[] args) {
+        Random rand = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите размерность массива: ");
         while (!sc.hasNextInt()) {
@@ -13,18 +17,16 @@ public class Task_2_replace {
             sc.next();
         }
         int n = sc.nextInt();
-
         double[] array = new double[n];
-        Random rand = new Random();
+
         for (int i = 0; i < array.length; i++) {
             array[i] = Math.ceil(rand.nextDouble() * 10000) / 100;
         }
         System.out.println("Первоночальный массив:");
-        Arrays.sort(array);
         System.out.println(Arrays.toString(array));
 
         System.out.print("Введите число Z: ");
-        while (!sc.hasNextInt()) {
+        while (!sc.hasNextDouble()) {
             System.out.print("Это не число! Введите число Z: ");
             sc.next();
         }
--- com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task3Count.java
+++ com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task3Count.java
@@ -1,11 +1,17 @@
 package com.upskill.basics.java.algorithmization.one_dimensional_arrays;
 
+//3. Дан массив действительных чисел, размерность которого N.
+//   Подсчитать, сколько в нем отрицательных, положительных и нулевых элементов.
+
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
-public class Task_3_count {
+public class Task3Count {
     public static void main(String[] args) {
+        Random random = new Random();
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Введите размерность массива: ");
         while (!sc.hasNextInt()) {
             System.out.print("Это не число! Введите размерность массива: ");
@@ -13,20 +19,21 @@ public class Task_3_count {
         }
         int n = sc.nextInt();
 
-        int[] array = new int[n];
+        double[] array = new double[n];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() *100 - 50);
+            array[i] = Math.ceil((random.nextDouble()  - 0.5)* 10000) / 100;
         }
+        System.out.println("Массив:");
         System.out.println(Arrays.toString(array));
 
         int countOtr = 0;
         int countPol = 0;
         int countZer = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
+        for (double number : array) {
+            if (number < 0) {
                 countOtr++;
-            } else if (array[i] > 0) {
+            } else if (number > 0) {
                 countPol++;
             } else countZer++;
         }
