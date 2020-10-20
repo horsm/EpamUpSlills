1eb65a948db861dce911bf6def1f6303c1db86d2
horsm <horsm@mail.ru>
2020-10-20 11:42:17 +0300

    Исправление

--- com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task5Print.java
+++ com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task5Print.java
@@ -3,10 +3,12 @@ package com.upskill.basics.java.algorithmization.one_dimensional_arrays;
 // 5. Даны целые числа а1 ,а2 ,..., аn . Вывести на печать только те числа, для которых аi > i.
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Task5Print {
     public static void main(String[] args) {
+        Random random = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите размерность массива: ");
         while (!sc.hasNextInt()) {
@@ -17,7 +19,7 @@ public class Task5Print {
 
         int[] array = new int[n];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100 - 50);
+            array[i] = random.nextInt(100) - 50;
         }
         System.out.println(Arrays.toString(array));
 
