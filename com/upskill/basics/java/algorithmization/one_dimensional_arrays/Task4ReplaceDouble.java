94742d807780247d95de8fbee01c474923580f41
horsm <horsm@mail.ru>
2020-10-20 09:03:11 +0300

    Исправление

--- com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task4ReplaceDouble.java
+++ com/upskill/basics/java/algorithmization/one_dimensional_arrays/Task4ReplaceDouble.java
@@ -4,8 +4,13 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Task_4_replace_double {
+public class Task4ReplaceDouble {
     public static void main(String[] args) {
+
+        int indexMax = 0;
+        int indexMin = 0;
+        double replace;
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите размерность массива: ");
         while (!sc.hasNextInt()) {
@@ -15,17 +20,29 @@ public class Task_4_replace_double {
         int n = sc.nextInt();
 
         double[] array = new double[n];
-        Random rand = new Random();
+        Random random = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = Math.ceil(rand.nextDouble() * 10000) / 100;
+            array[i] = Math.ceil((random.nextDouble()  - 0.5)* 10000) / 100;
         }
+        double min = array[0];
+        double max = array[0];
         System.out.println("Первоночальный массив:");
         System.out.println(Arrays.toString(array));
-        Arrays.sort(array);
 
-        double a = array[0];
-        array[0] = array[array.length - 1];
-        array[array.length - 1] = a;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                indexMin = i;
+            }
+            if (array[i] > max) {
+                max = array[i];
+                indexMax = i;
+            }
+        }
+        replace = array[indexMin];
+        array[indexMin] = array[indexMax];
+        array[indexMax] = replace;
 
         System.out.println("Массив с поменянными элементами: ");
         System.out.println(Arrays.toString(array));
