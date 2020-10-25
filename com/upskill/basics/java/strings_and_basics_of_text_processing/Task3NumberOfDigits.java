447bb843d5594d133f5e6dacf1ab1576897e3208
horsm <horsm@mail.ru>
2020-10-25 23:10:17 +0300

    Задача 3 - найти количество цифр

--- com/upskill/basics/java/strings_and_basics_of_text_processing/Task3NumberOfDigits.java
+++ com/upskill/basics/java/strings_and_basics_of_text_processing/Task3NumberOfDigits.java
@@ -6,14 +6,28 @@ import java.util.Scanner;
 
 public class Task3NumberOfDigits {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите строку: ");
-        String text = sc.next();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Введите строку: ");
+//        String text = sc.next();
 
-      //  String text = "Этот тект содержит 12345678 цифр. Нет, он содержит 9 цифр";
+        String text = "Этот тект содержит 12345678 цифр. Нет, он содержит 9 цифр";
+        System.out.println("Исходная строка: " + text);
 
-        long count = text.codePoints().filter(Character::isDigit).count();
-        System.out.println("Тест содержит " + count + " цифр.");
+        // Вариант 1
+//        long count = text.codePoints().filter(Character::isDigit).count();
+//        System.out.println("Тест содержит " + count + " цифр.");
 
+        // Вариант 2
+        System.out.println("Тест содержит " + getNumberDigits(text) + " цифр.");
+    }
+
+    public static int getNumberDigits(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
     }
 }
