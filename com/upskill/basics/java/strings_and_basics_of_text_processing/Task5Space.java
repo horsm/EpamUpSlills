e7bb21e7a15db262f02c01dc68de0d1563292d4e
horsm <horsm@mail.ru>
2020-10-25 22:42:15 +0300

    Задача 5 - удалить в строке все лишние пробелы

---
+++ com/upskill/basics/java/strings_and_basics_of_text_processing/Task5Space.java
@@ -0,0 +1,29 @@
+package com.upskill.basics.java.strings_and_basics_of_text_processing;
+
+// 5. Удалить в строке все лишние пробелы, то есть серии подряд идущих пробелов заменить на одиночные пробелы.
+//    Крайние пробелы в строке удалить.
+
+import java.util.ArrayList;
+
+public class Task5Space {
+    public static void main(String[] args) {
+        String originalString = "   Привет    Привет Привет  2 3 88 ";
+        String newString[] = originalString.split(" ");
+        ArrayList<String> stringList = new ArrayList<>();
+
+        System.out.println("Исходная строка: |" + originalString + "|");
+
+        for (int i = 0; i < newString.length; i++) {
+            if (!newString[i].equals("")) {
+                stringList.add(newString[i]);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < stringList.size() - 1; i++) {
+            stringBuilder.append(stringList.get(i)).append(" ");
+        }
+        stringBuilder.append(stringList.get(stringList.size() - 1));
+
+        System.out.println("Полученная строка: |" + stringBuilder.toString() + "|");
+    }
+}
