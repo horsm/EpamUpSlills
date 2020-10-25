603a309ddb653da7bf5c3345fb92c45c2e954a38
horsm <horsm@mail.ru>
2020-10-25 20:31:44 +0300

    Задача 2 - замена слов

---
+++ com/upskill/basics/java/strings_and_basics_of_text_processing/Task2Occurrences.java
@@ -0,0 +1,14 @@
+package com.upskill.basics.java.strings_and_basics_of_text_processing;
+
+import java.util.Scanner;
+
+public class Task2Occurrences {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String originalString = sc.next();
+
+        String value = originalString.replace("world", "letter");
+        System.out.println(value);
+    }
+}
