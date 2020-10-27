af7bc1f96112bfe2dc1843249b5c03db7f0b4be4
horsm <horsm@mail.ru>
2020-10-27 22:41:14 +0300

    Задача 2 - замена слова

--- com/upskill/basics/java/strings_and_basics_of_text_processing/string/Task2Occurrences.java
+++ com/upskill/basics/java/strings_and_basics_of_text_processing/string/Task2Occurrences.java
@@ -6,11 +6,30 @@ import java.util.Scanner;
 
 public class Task2Occurrences {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите строку: ");
-        String originalString = sc.next();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Введите строку: ");
+//        String originalString = sc.next();
 
-        String value = originalString.replace("world", "letter");
-        System.out.println(value);
+        // Вариант 1
+//        String value = originalString.replace("world", "letter");
+//        System.out.println(value);
+
+        // Вариант 2
+        StringBuilder strBuilder = new StringBuilder();
+        String text = "newsad word sdasdfsdafosoft word";
+        System.out.println("Исходный текст: " + text);
+        String letter;
+        for (int i = 0; i < text.length(); i++) {
+            if (i + 3 < text.length() && text.charAt(i) == 'w'
+                                      && text.charAt(i + 1) == 'o'
+                                      && text.charAt(i + 2) == 'r'
+                                      && text.charAt(i + 3) == 'd') {
+                strBuilder.append("letter");
+                i += 3;
+            } else {
+                strBuilder.append(text.charAt(i));
+            }
+        }
+        System.out.println("Новый текст: " + strBuilder.toString());
     }
 }
